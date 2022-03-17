d19b4813b3730ed4fb47ef1c9bb5752bed987845
gufengchangdao <96374798+gufengchangdao@users.noreply.github.com>
2022-03-17 11:35:40 +0800

    Add files via upload

---
+++ sketch_mar03b/sketch_mar03b.ino
@@ -0,0 +1,29 @@
+void setup() {
+// put your setup code here, to run once:
+pinMode(3,OUTPUT);
+}
+
+void loop() {
+// put your main code here, to run repeatedly:
+analogWrite(3,0);
+delay(100);
+
+analogWrite(3,50);
+delay(100);
+analogWrite(3,75);
+delay(100);
+analogWrite(3,100);
+delay(100);
+analogWrite(3,125);
+delay(100);
+analogWrite(3,150);
+delay(100);
+analogWrite(3,175);
+delay(100);
+analogWrite(3,200);
+delay(100);
+analogWrite(3,225);
+delay(100);
+analogWrite(3,250);
+delay(100);
+}
